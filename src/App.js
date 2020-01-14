0c71b4b1cb42b7f68c9d4c7645c6f4a4e99e77ae
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-01-14 09:38:38 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import App from './App.svelte';
+import svelte from 'svelte/compiler';
+import './App.scss';
+const app = new App({
+	target: document.body,
+	props: {
+		version: svelte.VERSION
+	}
+});
+
+export default app;
---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Navbar from "./Pages/Navbar.svelte"
+	import Layout from "./Pages/Layout.svelte"
+	import Table from "./components/Tables.svelte"
+	const navItems = ["Home", "Normline", "Masspec", "Timescan", "THz", "Powerfile", "Misc", "Settings"]
+
+</script>
+
+<style>
+	.pageContainer {
+		max-height: 70vh;
+		overflow-y: auto;
+	}
+</style>
+
+<Navbar {navItems}/>
+
+<div class="pageContainer">
+	{#each navItems as id}
+		<Layout {id} />
+	{/each}
+</div>
+<!-- <Table /> -->
