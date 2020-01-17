13099a7082bd219b6f7194f9c7c14531e3821f29
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-01-17 19:33:46 +0100

    clean code

--- src/testing/LineAnimate.svelte
+++ src/testing/LineAnimate.svelte
@@ -1,12 +1,6 @@
 <script>
   import LinearProgress from '@smui/linear-progress';
-  // import someAsyncAction from './someAsyncAction.js';
-
   let closed = true;
-
-  // someAsyncAction(() => {
-  //   closed = true;
-  // });
 </script>
 
 <div class="line" style=""><LinearProgress indeterminate {closed} /></div>
