4f7eae0d8af89b8b3e46a6a944f7dbee36cf21f1
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-01-20 22:44:49 +0100

    template for report

---
+++ public/assets/reports/template.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    document.getElementById("exportButton").addEventListener("click",()=>{alert("clicked!")})
+});
