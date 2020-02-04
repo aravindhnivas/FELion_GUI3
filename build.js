5ac19ed84f24ee40d6ceeb5196050408326642d9
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-02-04 21:17:59 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 var electronInstaller = require('electron-winstaller');
 var settings = {
-    appDirectory: '../github_project/release-builds/FELion_GUI3-win32-x64',
-    outputDirectory: '../github_project/release-builds/installer',
+    appDirectory: '../release-builds/FELion_GUI3-win32-x64',
+    outputDirectory: '../release-builds/installer',
     authors: 'Aravindh', loadingGif:"./loading.gif",
     exe: 'FELion_GUI3.exe', setupExe: "FELion_GUI3.exe",
     description: 'FELion Spectrum Analyser'
