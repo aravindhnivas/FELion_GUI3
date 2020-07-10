88c21b7f4a59352593b23d8f6981b62976a977ec
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-07-10 22:00:51 +0200

    remove unused libraries

--- static/assets/js/renderer.js
+++
@@ -1,6 +0,0 @@
-const customTitlebar = require('custom-electron-titlebar/lib/index');
-let MyTitleBar = new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex('#38236b'),
-    // icon: "./assets/images/icon.png"
-});
-MyTitleBar.updateTitle("FELion Spectrum Analyser");
