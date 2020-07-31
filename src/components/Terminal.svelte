b38b8907be80cc89e1c79e7a78a5a99229b95f65
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-07-31 18:26:48 +0200

    Update Terminal.svelte

--- src/components/Terminal.svelte
+++ src/components/Terminal.svelte
@@ -5,138 +5,140 @@
     import {createToast} from "./Layout.svelte";
     import PreModal from "./PreModal.svelte";
     import {tick} from "svelte";
+
     import Textfield from '@smui/textfield';
 
-    import CustomSwitch from '../components/CustomSwitch.svelte';
+    import CustomSwitch from './CustomSwitch.svelte';
+    ///////////////////////////////////////////////////////////////////////////////
 
-    export let commandToRun = localStorage["pythonpath"], commandArgsToRun = "-m, pip", commandResults = [], teminalFontSize=20;
+    const colorSets = {warning: "#ffdd57", danger:"#f14668", info:"#2098d1", normal:"#fafafa", success:"#20f996"}
 
-    let colorSets = {warning: "#ffdd57", danger:"#f14668", info:"#2098d1", normal:"#fafafa", success:"#20f996"}
+    export let commandToRun = localStorage["pythonpath"] || "", commandArgsToRun = "-m, pip", commandResults = [{color:colorSets.normal, results:">> "}], teminalFontSize=20;
 
-    // $: console.log(commandResults)
+    export let commandInputDiv = true, runShell = false;
     
     let preModal = {}
-
     let openShellTerminal = false;
 
-    async function terminalShell(){
-        await tick()
+    const srollTerminalDiv = async () => {
         const terminalDiv = document.getElementById("terminal")
-        
+        await tick()
+        const scrollTo = terminalDiv.scrollHeight - terminalDiv.clientHeight
+        terminalDiv.scrollTo({top:scrollTo, behavior: 'smooth'})
+    }
 
+    async function terminalShell(){
+        runShell = false
+        
+        await tick()
+        
+        srollTerminalDiv()
+        
         if (!commandToRun) {return createToast("No command entered", "warning")}
-
         commandResults = [...commandResults , {color:colorSets.normal, results:`>> ${commandToRun} ${commandArgsToRun.split(",").join(" ")}`}]
-        
         let ls;
 
+        
         try {
-            ls = spawn(commandToRun, commandArgsToRun.split(",").map(arg=>arg.trim()), { detached: true, stdio: 'pipe', shell: openShellTerminal });
 
+            ls = spawn(commandToRun, commandArgsToRun.split(",").map(arg=>arg.trim()), { detached: true, stdio: 'pipe', shell: openShellTerminal });
         } catch (error) {preModal.modalContent = error;  preModal.open = true}
 
-        ls.stdout.on("data", async (data) => {
-            
+        ls.stdout.on("data", data => {
             commandResults = [...commandResults, {color:colorSets.info, results:`>> ${data || ""}`}]
-            await tick()
-            const scrollTo = terminalDiv.scrollHeight - terminalDiv.clientHeight
+            srollTerminalDiv()
 
-
-            terminalDiv.scrollTo({top:scrollTo, behavior: 'smooth'})
-        
         })
-
         
-        ls.stderr.on("data", async (data) => { 
+        ls.stderr.on("data", data => { 
             commandResults = [...commandResults, {color:colorSets.danger, results:`>> ${data || ""}`}]
 
-            await tick()
-            const scrollTo = terminalDiv.scrollHeight - terminalDiv.clientHeight
-            terminalDiv.scrollTo({top:scrollTo, behavior: 'smooth'})
-        
+            srollTerminalDiv()
         })
-        
-        ls.on("close", async (code) => {  
+
+
+        ls.on("close", code => {  
+
             commandResults = [...commandResults, {color: code === 1 ? colorSets.danger :  colorSets.success, results:`>> child process exited with code ${code}`}]
-            
-            await tick()
-            const scrollTo = terminalDiv.scrollHeight - terminalDiv.clientHeight
-            terminalDiv.scrollTo({top:scrollTo, behavior: 'smooth'})
+            srollTerminalDiv()
 
             const outputLog = `${new Date().toLocaleString()}\n\n-----------------------------------------\nRunning terminal commands\n${commandResults.map(cmd=>cmd.results).join("")}\n-----------------------------------------\n`
-
-            try {
-                fs.writeFileSync(path.resolve(__dirname, "output.log"), outputLog)
-
-            } catch (error) { createToast("Could not save the outputs to file: output.log", "warning")}
+            
+            try { fs.writeFileSync(path.resolve(__dirname, "output.log"), outputLog)} 
+            
+            catch (error) { createToast("Could not save the outputs to file: output.log", "warning")}
 
         })
-
     }
-    
+
+    $: if(runShell) terminalShell()
+
 </script>
 
-
 <style>
+
     .box { background-color: #6a50ad8a; overflow-y: auto; height: calc(100vh - 12em);}
 
     #terminal {
-        height: 75%;
 
         margin-bottom: 1em;
         background-color: #4a4a4ae6;
+
         user-select: text;
+    
     }
 
     .contentBox {
 
         max-height: 70vh;
         overflow: auto;
+    
         padding-bottom: 1em;
         padding: 1em;
         height: calc(100vh - 12em);
 
     }
 
+
 </style>
+
 
 <PreModal bind:preModal/>
 
 
 <div class="content contentBox terminalBox" >
 
-    <div class="commandInput">
-    
-        <div class="run" style="display:flex; align-items:center; margin-bottom:1em;">
-            <Textfield  bind:value={commandToRun} label="Enter command to run"/>
-            <Textfield  bind:value={commandArgsToRun} label="Enter command-arg"/>
-        </div>
+    {#if commandInputDiv}
 
-        <div class="run" style="display:flex; align-items:center; margin-bottom:1em;">
-            <IconButton class="material-icons" on:click={terminalShell}>play_arrow</IconButton>
+        <div class="commandInput">
+            <div class="run" style="display:flex; align-items:center; margin-bottom:1em;">
 
-            <CustomSwitch style="margin: 0 1em;" bind:selected={openShellTerminal} label="Shell"/>
-            <Textfield type="number" step="1" min="0" bind:value={teminalFontSize} variant="outlined" style="width:7em" label="Font Size"/>
-            
-            <IconButton class="material-icons is-pulled-right" style="background: #f14668; border-radius: 2em;" on:click="{()=>commandResults=[{color:colorSets.normal, results:`>> cleared`}] }">clear</IconButton>
+                <Textfield  bind:value={commandToRun} label="Enter command to run"/>
 
+                <Textfield  bind:value={commandArgsToRun} label="Enter command-arg"/>
+            </div>
 
-        </div>
-    
-    </div>
+            <div class="run" style="display:flex; align-items:center; margin-bottom:1em;">
+                
+                <IconButton class="material-icons" on:click={terminalShell}>play_arrow</IconButton>
+                <CustomSwitch style="margin: 0 1em;" bind:selected={openShellTerminal} label="Shell"/>
+                <Textfield type="number" step="1" min="0" bind:value={teminalFontSize} variant="outlined" style="width:7em" label="Font Size"/>
 
-    <div class="box" id="terminal">
-
-        <div>
-
-            <div > 
-                {#each commandResults as {color, results}}
-
-                        <h1 class="subtitle" style="color:{color}; font-size:{teminalFontSize}px; white-space: pre-wrap; ">{results}</h1>
-                {/each}
-
+                <IconButton class="material-icons is-pulled-right" style="background: #f14668; border-radius: 2em;" on:click="{()=>commandResults=[{color:colorSets.normal, results:`>> cleared`}] }">clear</IconButton>
             </div>
         </div>
-    
+
+    {/if}
+
+
+
+    <div class="box" id="terminal" style="height: {commandInputDiv ? 75 : 90}%;">
+
+        {#each commandResults as {color, results}}
+            <h1 class="subtitle" style="color:{color}; font-size:{teminalFontSize}px; white-space: pre-wrap; ">{results}</h1>
+
+        {/each}
+
     </div>
+
 </div>
