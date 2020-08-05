09801b7343684307c11c1b42e893313edf1e969d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-08-06 00:51:15 +0200

    Update CustomCheckList.svelte

--- src/components/CustomCheckList.svelte
+++ src/components/CustomCheckList.svelte
@@ -2,20 +2,21 @@
     
     import List, {Item, Meta, Label} from '@smui/list';
     import Checkbox from '@smui/checkbox';
-    import VirtualList from '@sveltejs/svelte-virtual-list';
-
-    export let items=[], fileChecked=[], height="500px", style="";
+    export let items=[], fileChecked=[], style="";
 
 </script>
 
 <div {style}>
-    <VirtualList {items} let:item {height}>
-
         <List checklist>
-            <Item style="height:2.5em;">
-                <Label>{item.name}</Label>
-                <Meta><Checkbox bind:group={fileChecked} value={item.name} on:click/></Meta>
-            </Item>
+            {#each items as item}
+                <Item style="height:2.5em;">
+        
+                    <Label>{item.name}</Label>
+        
+                    <Meta><Checkbox bind:group={fileChecked} value={item.name} on:click/></Meta>
+
+                </Item>
+                
+            {/each}
         </List>
-    </VirtualList>
 </div>
