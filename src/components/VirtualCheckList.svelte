09d407ada6ff20f756747ccc95dcbbb951f7a51e
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-08-06 00:51:13 +0200

    Create VirtualCheckList.svelte

---
+++ src/components/VirtualCheckList.svelte
@@ -0,0 +1,21 @@
+<script>
+    
+    import List, {Item, Meta, Label} from '@smui/list';
+    import Checkbox from '@smui/checkbox';
+    import VirtualList from '@sveltejs/svelte-virtual-list';
+
+    export let items=[], fileChecked=[], height="500px", style="";
+
+</script>
+
+<div {style}>
+    <VirtualList {items} let:item {height}>
+
+        <List checklist>
+            <Item style="height:2.5em;">
+                <Label>{item.name}</Label>
+                <Meta><Checkbox bind:group={fileChecked} value={item.name} on:click/></Meta>
+            </Item>
+        </List>
+    </VirtualList>
+</div>
