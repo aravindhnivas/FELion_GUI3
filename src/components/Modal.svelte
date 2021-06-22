a9933371dd6c6cb523c73ca0184e03f9c4f2b8d9
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-06-22 08:14:26 +0200

    Update Modal.svelte

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -13,27 +13,27 @@
   
   .delete {background-color: #fafafa;}
   .delete:hover {background-color: #f14668;}
+  .modal-card-title {margin:0!important;}
+  
 </style>
 
+
 <svelte:window on:keydown="{(e)=> {if(e.keyCode===27) active=false}}"/>
-
 <div class="modal" class:is-active={active}>
-
   <div class="modal-background"></div>
-
-
   <div class="modal-card animated fadeIn faster" {style}>
+
 
     <header class="modal-card-head">
       <p class="modal-card-title">{title}</p>
       <span class="delete is-pulled-right" on:click="{()=>{active=false; dispatch('closed', {active}) }}"></span>
-
     </header>
 
     <section class="modal-card-body" style="background: {bodyBackground}; {bodyStyle}" id="{contentID||window.getID()}"><slot name="content" style="white-space: pre-wrap;"/></section>
 
     {#if $$slots.footerbtn}
-      <footer class="modal-card-foot">
+
+    <footer class="modal-card-foot">
         <div style="margin-left:auto; display:flex;">
           <slot name="footerbtn" /> 
 
