27dadb07390301dc5dde7867a16455dbe0bef80b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-06-28 23:02:24 +0200

    collisional simulation notebooks

--- src/Pages/thz/windows/CollisionalDistribution.svelte
+++ src/Pages/thz/windows/CollisionalDistribution.svelte
@@ -3,7 +3,7 @@
     import SeparateWindow from "../../../components/SeparateWindow.svelte";
     import { plot } from "../../../js/functions";
     import boltzman_distribution from "../functions/boltzman_distribution";
-    import collisional_cooling from "../functions/collisional_cooling";
+    // import collisional_cooling from "../functions/collisional_cooling";
     import Textfield from '@smui/textfield';
     // import {onMount} from 'svelte';
 
@@ -11,7 +11,7 @@
     export let collisionalRateConstants, energyLevels, electronSpin, zeemanSplit, energyUnit;
     
     
-    let initialTemp = 300;
+    let initialTemp = 300, duration=600;
 
     let numberDensity="2e14";
 
@@ -22,10 +22,28 @@
     let graphWindow=null, windowReady=false;
     let stepSize=0.1;
 
-    function computeCollisionalProcess() {
-        const boltzmanDistribution = boltzman_distribution({energyLevels, trapTemp:initialTemp, electronSpin, zeemanSplit, energyUnit})
-        const collisionalCooling = collisional_cooling({collisionalRateConstants, boltzmanDistribution, numberDensity})
-        console.log(collisionalCooling)
+    async function computeCollisionalProcess(e) {
+        try {
+            const boltzmanDistribution = boltzman_distribution({energyLevels, trapTemp:initialTemp, electronSpin, zeemanSplit, energyUnit})
+            
+            const boltzmanDistributionValues = {}
+            boltzmanDistribution.forEach(f=>boltzmanDistributionValues[f.label]=f.value)
+
+            const collisionalRateConstantValues = {}
+            collisionalRateConstants.forEach(f=>collisionalRateConstantValues[f.label]=f.value)
+
+
+            const pyfile = "collisionalSimulation.py";
+            
+            const args=[JSON.stringify(
+                {numberDensity, boltzmanDistributionValues, duration, collisionalRateConstantValues}
+            )]
+            const dataFromPython = await computePy_func({e, pyfile, args})
+
+            console.log(dataFromPython)
+        } catch (error) {
+            window.createToast(error, "danger")
+        }
     }
     $: if (windowReady) {setTimeout(()=>graphWindow.focus(), 100)}
 
@@ -50,6 +68,7 @@
                 
                 <Textfield bind:value={stepSize} label="stepSize" style="width:auto;"/>
                 <Textfield bind:value={initialTemp} label="temperature" input$type="number" input$step={stepSize} input$min=0 style="width:auto;"/>
+                <Textfield bind:value={duration} label="duration (in ms)" input$type="number" input$step={stepSize} input$min=0 style="width:auto;"/>
                 <Textfield bind:value={numberDensity} label="Number density" style="width:auto;"/>
                 <button class="button is-link" on:click={computeCollisionalProcess}>Compute</button>
             </div>
