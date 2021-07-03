58f88f1cad25ca142336894610910331fc54d991
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-07-03 23:30:55 +0200

    Update constants.js

--- src/Pages/thz/functions/constants.js
+++ src/Pages/thz/functions/constants.js
@@ -1,3 +1,5 @@
 
 export const PlanksConstant = 6.62607004e-34 // J.s
 export const SpeedOfLight = 299792458 // m/s
+export const boltzmanConstant = 1.38064852e-23 // in J.K-1
+export const boltzmanConstantInWavenumber = boltzmanConstant/1.98630e-23 // in cm-1
