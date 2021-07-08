bf70265379a37a0a308b6543066b64c50b47d680
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-07-08 22:12:01 +0200

    Update collisionalFit.py

--- static/assets/python_files/ROSAA/collisionalFit.py
+++ static/assets/python_files/ROSAA/collisionalFit.py
@@ -6,25 +6,21 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 np.seterr(invalid="ignore")
 
-# import yaml
-
 main_module_loc = str(pt(__file__).joinpath("../../"))
 sys.path.insert(0, main_module_loc)
-
 from FELion_definitions import sendData
-
 from FELion_constants import pltColors
 
 def linearFit(x, m, c): return m*x + c
 
-
 def collisionalFit():
 
     fullname = pt(args["collisionlFile"])
+
+
     with open(fullname, "r") as f:
         data_info = f.readlines()
-    
-    # print(data_info, flush=True)
+
     data = []
     for line in data_info:
         if not "#" in line:
@@ -32,23 +28,20 @@ def collisionalFit():
                 line = line.strip()
                 line = line.split("\t")
                 data.append(line)
-                # print(data, flush=True)
 
-    # data = np.genfromtxt(fullname)
     temperature = np.array(data[0], dtype=float)
-
     fitTemp = np.arange(1, temperature[-1]+0.1, 1)
     collisionalTemp = float(args["collisionalTemp"])
     collisionalRateType = args["collisionalRateType"]
     dataToSend = {"type" : collisionalRateType, "rateConstants" : []}
-    # dataToSend = {}
+
     fig, ax = plt.subplots(figsize=(10, 6), dpi=100)
 
     size = 2+len(temperature)
     for counter, rate in enumerate(data[1:]):
-        # print(f"{rate=}", flush=True)
         i, j = rate[:2]
         
+
         rateConstants = np.array(rate[2:size], dtype=float)
         ax.plot(temperature, rateConstants, ".", c=pltColors[counter])
         pop, pcov = curve_fit(linearFit, temperature, rateConstants)
@@ -56,18 +49,16 @@ def collisionalFit():
 
         lg = f"{i.strip()} --> {j.strip()}"
         # lg += f" [m={pop[0]:.1f}({perr[0]:.1f}), c={pop[1]:.1f}({perr[1]:.1f})]"
-
-
         ax.plot(fitTemp, linearFit(fitTemp, *pop), "-", c=pltColors[counter], label=lg)
+
         _requiredRate = {"label": lg, "value": float(linearFit(collisionalTemp, *pop))}
+
         dataToSend["rateConstants"].append(_requiredRate)
-
     ax.legend()
-
 
     ax.set(xlabel="Temp (K)", ylabel="k$_{jj'}$ (cm$^3$ molecule$^{-1}$ s$^{-1}$)", title="Collisional rate constants", yscale="log")
 
-    saveFilename = args["saveFilename"]
+    # saveFilename = args["saveFilename"]
 
     # with open(fullname.parent/f"{saveFilename}_{requiredTemp}K.json", 'w+') as f:
     #     saveData = json.dump(dataToSend, f, sort_keys=True, indent=4, separators=(',', ': '))
@@ -75,10 +66,9 @@ def collisionalFit():
     # plt.show()
 
 if __name__ == "__main__":
-
     args = json.loads(sys.argv[1])
-    
     print(args, flush=True)
 
     pp = pprint.PrettyPrinter(indent=4)
+    
     collisionalFit()
