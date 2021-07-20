54f30e62521985191a3000dcfb344da5a0bc5285
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-07-21 01:15:29 +0200

    Update ROSAA_simulation.py

--- static/assets/python_files/ROSAA/ROSAA_simulation.py
+++ static/assets/python_files/ROSAA/ROSAA_simulation.py
@@ -1,5 +1,5 @@
 
-import sys, json, pprint, time
+import sys, json, pprint, time, os
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path as pt
@@ -20,50 +20,93 @@ class ROSAA:
 
     def __init__(self):
 
-        self.energyLevels = {key : float(value) for key, value in conditions["energy_levels"].items()}
+        self.energyLevels = {key: float(value) for key, value in conditions["energy_levels"].items()}
         self.energyKeys = list(self.energyLevels.keys())
         self.lineshape_conditions = conditions["lineshape_conditions"]
         self.collisionalTemp = float(self.lineshape_conditions["IonTemperature(K)"])
-        self.collisionalRates = {key : float(value) for key, value in conditions["collisional_rates"].items()}
+        
+        self.collisionalRateConstants = {key: float(value) for key, value in conditions["collisional_rates"].items()}
         self.includeSpontaneousEmission = conditions["includeSpontaneousEmission"]
         self.includeCollision = conditions["includeCollision"]
-
         if self.includeSpontaneousEmission:
-            self.einsteinA_Rates = {key : float(value) for key, value in conditions["einstein_coefficient"]["A"].items()}
-            self.einsteinB_Rates = {key : float(value) for key, value in conditions["einstein_coefficient"]["B"].items()}
+            self.einsteinA_Rates = {key: float(value) for key, value in conditions["einstein_coefficient"]["A"].items()}
+            self.einsteinB_Rates = {key: float(value) for key, value in conditions["einstein_coefficient"]["B"].items()}
 
 
         self.excitedFrom = conditions["excitedFrom"]
         self.excitedTo = conditions["excitedTo"]
         self.transitionLevels = [self.excitedFrom, self.excitedTo]
 
-        start_time = time.perf_counter()
-        self.Simulate()
-        end_time = time.perf_counter()
+        self.start_time = time.perf_counter()
+        variable = conditions["variable"]
+        variableRange = conditions["variableRange"]
 
-
-        print(f"Total simulation time {(end_time - start_time):.2f} s", flush=True)
-
-        self.molecule = conditions["main_parameters"]["molecule"]
-        self.taggingPartner = conditions["main_parameters"]["tagging partner"]
-
-        self.legends = [f"${self.molecule}$ ({key.strip()})" for key in self.energyKeys]
-        if self.includeAttachmentRate:
-            self.legends += [f"${self.molecule}${self.taggingPartner}"]
-            self.legends += [f"${self.molecule}${self.taggingPartner}$_{i+1}$" for i in range(1, self.totalAttachmentLevels)]
-        self.writeFile = conditions["writefile"]
         
-        if self.writeFile:
-            self.WriteData()
+        if variable == "time":
 
-        self.Plot()
+            nHe = float(conditions["numberDensity"])
+            self.Simulate(nHe)
+            
 
-    def SimulateODE(self, t, counts):
+            self.molecule = conditions["main_parameters"]["molecule"]
+            self.taggingPartner = conditions["main_parameters"]["tagging partner"]
+
+            self.legends = [f"${self.molecule}$ ({key.strip()})" for key in self.energyKeys]
+            if self.includeAttachmentRate:
+                self.legends += [f"${self.molecule}${self.taggingPartner}"]
+                self.legends += [f"${self.molecule}${self.taggingPartner}$_{i+1}$" for i in range(1, self.totalAttachmentLevels)]
+            self.writeFile = conditions["writefile"]
+            
+            if self.writeFile:
+                self.WriteData()
+            self.Plot()
+        
+        elif variable == "He density(cm3)":
+            signalList = []
+            _start, _end, _steps = variableRange.split(",")
+
+            dataList = np.linspace(float(_start), float(_end), int(_steps))
+            for counter, _nHe in enumerate(dataList):
+                print(f"{counter+1}/{int(_steps)}: {_nHe=:.2e}", flush=True)
+
+                self.Simulate(_nHe)
+                
+                if self.includeAttachmentRate:
+                    signal_index = len(self.energyKeys)+1
+                
+                    signal = (1 - (self.lightON_distribution[signal_index][1:] / self.lightOFF_distribution[signal_index][1:]))*100
+
+                    signal = round(signal[-1], 1)
+                    signalList.append(signal)
+
+            
+            signalList = np.nan_to_num(signalList).clip(min=0)
+            print(signalList, flush=True)
+
+            location = pt(conditions["currentLocation"])/"OUT"
+            if not location.exists():
+                os.mkdir(location)
+
+            savefilename = conditions["savefilename"]
+            with open(location / f"{savefilename}_ROSAA_output_nHe_{_start}_{_end}_{int(_steps)}.dat", 'w+') as f:
+
+                f.write("# nHe(cm-3)\tSignal(%)\n")
+
+                for x, y in zip(dataList, signalList):
+                    f.write(f"{x:.2e}\t{y:.2f}\n")
+                print(f"{savefilename} file written in {location} folder.", flush=True)
+            
+            fig, ax = plt.subplots(figsize=(10, 6), dpi=100)
+            ax.plot(dataList, signalList, ".-")
+            ax = optimizePlot(ax, xlabel="number Density ($cm^{-3}$)", ylabel="Signal (%)")
+            ax.set_xscale("log")
+
+            plt.show()
+
+    def SimulateODE(self, t, counts, nHe):
         
         if self.includeAttachmentRate:
-
             N = counts[:-self.totalAttachmentLevels]
-
             N_He = counts[-self.totalAttachmentLevels:]
         else: N = counts
 
@@ -81,8 +124,8 @@ class ROSAA:
                     keyInverse = f"{i} --> {j}"
 
                     if self.includeCollision:
-                        if key in self.collisionalRates and keyInverse in self.collisionalRates:
-                            R_coll = self.collisionalRates[key]*N[j] - self.collisionalRates[keyInverse]*N[i]
+                        if key in self.collisionalRateConstants and keyInverse in self.collisionalRateConstants:
+                            R_coll = (self.collisionalRateConstants[key]*N[j] - self.collisionalRateConstants[keyInverse]*N[i])*nHe
                             collisional.append(R_coll)
 
                     if self.includeSpontaneousEmission:
@@ -110,14 +153,14 @@ class ROSAA:
             dR_dt.append(temp)
 
         if self.includeAttachmentRate:
-            dR_dt = self.compute_attachment_process(N_He, dR_dt)
+            dR_dt = self.compute_attachment_process(N_He, dR_dt, nHe)
         return dR_dt
 
-    def compute_attachment_process(self, N_He, N):
+    def compute_attachment_process(self, N_He, N, nHe):
         
         N = {key:value for key, value in zip(self.energyKeys, N)}
-        attachmentRate0 = - self.k3[0]*N[self.excitedFrom] + self.kCID[0]*N_He[0]*self.kCID_branch
-        attachmentRate1 = - self.k31_excited*N[self.excitedTo] + self.kCID[0]*N_He[0]*(1-self.kCID_branch)
+        attachmentRate0 = - self.k3[0]*nHe**2*N[self.excitedFrom] + self.kCID[0]*nHe*N_He[0]*self.kCID_branch
+        attachmentRate1 = - self.k31_excited*nHe**2*N[self.excitedTo] + self.kCID[0]*nHe*N_He[0]*(1-self.kCID_branch)
         
         N[self.excitedFrom] += attachmentRate0
         N[self.excitedTo] += attachmentRate1
@@ -128,7 +171,7 @@ class ROSAA:
         currentRate =  - attachmentRate0 - attachmentRate1
 
         for i in range(self.totalAttachmentLevels-1):
-            nextRate = - self.k3[i+1]*N_He[i] + self.kCID[i+1]*N_He[i+1]
+            nextRate = - self.k3[i+1]*nHe**2*N_He[i] + self.kCID[i+1]*nHe*N_He[i+1]
             attachmentRate = currentRate + nextRate
             dR_dt.append(attachmentRate)
             currentRate = -nextRate
@@ -137,7 +180,6 @@ class ROSAA:
         dR_dt.append(currentRate)
 
         return dR_dt
-
 
     def GetAttachmentRatesParameters(self):
         self.attachment_rate_coefficients = conditions["attachment_rate_coefficients"]
@@ -154,9 +196,9 @@ class ROSAA:
         self.totalAttachmentLevels = int(self.attachment_rate_coefficients["totalAttachmentLevels"])
         self.includeAttachmentRate = conditions["includeAttachmentRate"]
     
-    def Simulate(self):
+    def Simulate(self, nHe):
+
         self.simulation_parameters = conditions["simulation_parameters"]
-        
         duration = self.simulation_parameters["Simulation time(ms)"]
         duration = float(duration)*1e-3 # converting ms ==> s
 
@@ -170,12 +212,12 @@ class ROSAA:
         self.boltzmanDistribution = boltzman_distribution( self.energyLevels, initialTemp, self.electronSpin, self.zeemanSplit )
 
         self.GetAttachmentRatesParameters()
-
         N_He = []
+
         if self.includeAttachmentRate:
         
             N_He = self.totalAttachmentLevels*[0]
-            print(f"{N_He=}", flush=True)
+            # print(f"{N_He=}", flush=True)
 
         totalSteps = int(self.simulation_parameters["Total steps"])
         initialDuration = 5e-3
@@ -188,14 +230,18 @@ class ROSAA:
             self.simulateTime = np.linspace(0, duration, int(totalSteps*0.5))
 
         self.lightON=False
-        N_OFF = solve_ivp(self.SimulateODE, tspan, [*self.boltzmanDistribution, *N_He], dense_output=True)
-
+        start_time = time.perf_counter()
+        N_OFF = solve_ivp(self.SimulateODE, tspan, [*self.boltzmanDistribution, *N_He], args=(nHe,), dense_output=True)
         self.lightOFF_distribution = N_OFF.sol(self.simulateTime)
         self.lightON=True
 
+        N_ON = solve_ivp(self.SimulateODE, tspan, [*self.boltzmanDistribution, *N_He], args=(nHe, ), dense_output=True)
         
-        N_ON = solve_ivp(self.SimulateODE, tspan, [*self.boltzmanDistribution, *N_He], dense_output=True)
         self.lightON_distribution = N_ON.sol(self.simulateTime)
+
+        end_time = time.perf_counter()
+        print(f"Current simulation time {(end_time - start_time):.2f} s", flush=True)
+        print(f"Total simulation time {(end_time - self.start_time):.2f} s", flush=True)
         
     def Plot(self):
 
@@ -251,11 +297,8 @@ class ROSAA:
             print(f"{savefilename} file written in {location} folder.", flush=True)
 
 if __name__ == "__main__":
-    
     conditions = json.loads(sys.argv[1])
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(conditions)
-
     sys.stdout.flush()
-
     ROSAA()
