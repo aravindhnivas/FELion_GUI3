9f7559dc5ea6e5cbad423112863e3ab1b24038e7
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-08-03 17:51:46 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -8,51 +8,43 @@ import postcss from 'rollup-plugin-postcss';
 
 import json from '@rollup/plugin-json';
 import yaml from '@rollup/plugin-yaml';
-import css from 'rollup-plugin-css-only';
+
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
 	input: 'src/App.js',
-
-	output: [
-		{ sourcemap: true, format: 'cjs', name: 'app', file: 'static/bundle.js' },
-		// { sourcemap: true, format: 'cjs', name: 'app', file: 'static/bundle.min.js', plugins: [terser()] },
-
-	],
+	output: [ { sourcemap: true, format: 'cjs', name: 'app', file: 'static/bundle.js' } ],
 
 	plugins: [
+
+
 		svelte({
-			emitCss: true,
+			emitCss: false,
 			compilerOptions: {
-				// enable run-time checks when not in production
-				dev: !production
+				dev: !production,
+				css: css => { css.write('bundle.css'); },
 			},
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
+		
 			preprocess: autoPreprocess()
 		}),
-		css({ output: 'bundle.css' }),
 		resolve({ dedupe: ['svelte', 'svelte/transition', 'svelte/internal'] }),
 		commonjs(),
 		!production && livereload('static'),
-
 		production && terser(),
 		postcss({
 			extract: true,
 			minimize: true,
 
-			
 			sourceMap: true,
-			use: [
-				['sass', {
-					includePaths: ['./src/theme', './node_modules']
-				}]
-			]
+
+			use: [ ['sass', { includePaths: ['./src/theme', './node_modules'] }] ]
 		}),
-
 		json(), yaml()
-	],
 
+	
+	],
+	
 	watch: { clearScreen: false },
 	external: ['electron', 'child_process', 'fs', 'path', 'url', 'module', 'os']
+
 };
