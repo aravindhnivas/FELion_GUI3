efe16908248fb5d52b503d3e0f2b9d0f4b851d25
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-08-05 11:44:00 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,49 +9,45 @@ import postcss from 'rollup-plugin-postcss';
 import json from '@rollup/plugin-json';
 import yaml from '@rollup/plugin-yaml';
 import alias from '@rollup/plugin-alias';
+
+
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
+
 	input: 'src/App.js',
+
+
 	output: [ { sourcemap: true, format: 'cjs', name: 'app', file: 'static/bundle.js' } ],
-
 	plugins: [
-
-
 		alias({
 			entries: [
 				{ find: 'src', replacement: './src' },
 				{ find: 'components', replacement: './src/components' }
 			]
 		}),
-
-
 		svelte({
-			emitCss: false,
-			compilerOptions: {
-				dev: !production,
-				css: css => { css.write('bundle.css'); },
-			},
-		
+			emitCss: true,
+			compilerOptions: {dev: !production},
 			preprocess: autoPreprocess()
 		}),
 		resolve({ dedupe: ['svelte', 'svelte/transition', 'svelte/internal'] }),
+
 		commonjs(),
+
 		!production && livereload('static'),
+
 		production && terser(),
 		postcss({
-			extract: true,
+			extract: 'bundle.css',
+
 			minimize: true,
-
 			sourceMap: true,
-
 			use: [ ['sass', { includePaths: ['./src/theme', './node_modules'] }] ]
 		}),
 		json(), yaml()
 
-	
 	],
-	
 	watch: { clearScreen: false },
 	external: ['electron', 'child_process', 'fs', 'path', 'url', 'module', 'os']
 
