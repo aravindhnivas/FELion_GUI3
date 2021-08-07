96c8faa0c5e21909934b95d96b8e35f5f6f5158d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-08-07 20:57:33 +0200

    felion icon

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const { app, BrowserWindow } = require('electron');
-
+const path = require("path");
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1200, height: 700, frame: false,
+    width: 1200, height: 700, frame: false, icon: path.resolve(__dirname, "assets/logo/felion_icon.icns"),
     webPreferences: {
       nodeIntegration: true, contextIsolation: false, nativeWindowOpen: true, webviewTag: true, nodeIntegrationInWorker: true, enableRemoteModule: true
     },
