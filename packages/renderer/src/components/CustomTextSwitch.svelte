fa793ff2ebb992da8247885609a1f9cdc6cf9b75
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2021-12-11 22:33:55 +0100

    Create CustomTextSwitch.svelte

---
+++ packages/renderer/src/components/CustomTextSwitch.svelte
@@ -0,0 +1,33 @@
+<script>
+
+    import Textfield from '@smui/textfield';
+
+    export let min=0;
+    export let value="";
+    export let label="";
+    export let style=""
+    export let variant="standard"
+    let step=1;
+    let changeStepsize = false
+</script>
+
+{#if changeStepsize}
+    
+    <Textfield
+        bind:value={step} {style} {variant}
+        label="enter stepSize"  
+        on:keyup = { ({key}) => {if(key=="Enter"){changeStepsize=false;} } }
+        on:click = { ({shiftKey}) => {if(shiftKey){changeStepsize=false;} } }
+    />
+
+{:else}
+    <Textfield bind:value {style} {variant}
+        {label} 
+        on:keyup
+        on:change
+        on:click = { ({shiftKey}) => { if(shiftKey){changeStepsize=true} } }
+        input$min={min}
+        input$step={step}
+        input$type="number"
+    />
+{/if}
