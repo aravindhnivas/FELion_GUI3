ec564020e114173c59d4728f25b01a492a738984
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-02-23 00:35:27 +0100

    Update THz.svelte

--- packages/renderer/src/Pages/THz.svelte
+++ packages/renderer/src/Pages/THz.svelte
@@ -6,10 +6,9 @@
     import CustomCheckbox       from "$components/CustomCheckbox.svelte"
     import CustomIconSwitch     from "$components/CustomIconSwitch.svelte"
     import {plot}               from "../js/functions.js"
-    // import ROSAA                from "./thz/ROSAA.svelte"
     import {relayout, restyle}  from 'plotly.js/dist/plotly-basic';
     import computePy_func       from "$src/Pages/general/computePy"
-    
+    // import Dygraph              from 'dygraphs';
     /////////////////////////////////////////////////////////////////////////
 
     // Initialisation
@@ -31,11 +30,9 @@
 
     const changePlotStyle = () => { restyle("thzPlot", {mode:plotStyleSelected, fill: plotFill ? "tozeroy" : ""})}
 
-    function plotData({e=null, filetype="thz", tkplot=false, justPlot=false, general={} }={}){
+    async function plotData({e=null, filetype="thz", tkplot=false, justPlot=false, general={} }={}){
 
         if (fileChecked.length === 0 && filetype === "thz") {return window.createToast("No files selected", "danger")}
-
-    
         const thz_args = {thzfiles, binData, tkplot, fG, fL, binSize, justPlot, saveInMHz}
         let pyfileInfo = {general,
             thz: {pyfile:"thz_scan" , args:[JSON.stringify(thz_args)]},
@@ -47,27 +44,26 @@
             return computePy_func({e, pyfile, args, general:true, openShell})
 
         }
-        computePy_func({e, pyfile, args})
-            .then((dataFromPython)=>{
-                if (filetype=="thz") {
+        const dataFromPython = await computePy_func({e, pyfile, args})
+        if (filetype=="thz") {
 
-                    plot(`THz Scan: Depletion (%)`, "Frequency (GHz)", "Depletion (%)", dataFromPython["thz"], "thzPlot")
-                    plot(`THz Scan`, "Frequency (GHz)", "Counts", dataFromPython["resOnOff_Counts"], "resOnOffPlot")
-                    if (!justPlot) {
-                        let lines = [];
-                        for (let x in dataFromPython["shapes"]) { lines.push(dataFromPython["shapes"][x]) }
-                        let layout_update = { shapes: lines }
-                        relayout("thzPlot", layout_update)
-                    }
-                }
-                window.createToast("Graph plotted", "success")
-                graphPlotted = true
-            })
+            plot(`THz Scan: Depletion (%)`, "Frequency (GHz)", "Depletion (%)", dataFromPython["thz"], "thzPlot")
+            plot(`THz Scan`, "Frequency (GHz)", "Counts", dataFromPython["resOnOff_Counts"], "resOnOffPlot")
+            if (!justPlot) {
+                let lines = [];
+                for (let x in dataFromPython["shapes"]) { lines.push(dataFromPython["shapes"][x]) }
+                let layout_update = { shapes: lines }
+                relayout("thzPlot", layout_update)
+            }
+        
+        }
+        window.createToast("Graph plotted", "success")
+        graphPlotted = true
     }
 
 </script>
 
-<Layout  {filetype} {graphPlotted} {id} bind:currentLocation bind:fileChecked>
+<Layout  {filetype} {graphPlotted} {id} bind:currentLocation bind:fileChecked >
 
     <svelte:fragment slot="buttonContainer">
         <div class="align v-center">
@@ -80,27 +76,20 @@
             <CustomTextSwitch bind:value={binSize} label="binSize (kHz)" step="0.1" />
             <CustomTextSwitch bind:value={fG} label="fG (MHz)" step="0.1" />
             <CustomTextSwitch bind:value={fL} label="fL (MHz)" step="0.1" />
-            <!-- <button class="{btnClass}" on:click="{()=>{ROSAA_modal_active=true}}">ROSAA mode</button> -->
-
         </div>
-
     </svelte:fragment>
 
     <svelte:fragment slot="plotContainer">
-
-        <!-- Plots -->
         <div id="resOnOffPlot" class="graph__div" ></div>
-
         {#if graphPlotted}
-
             <div class="animated fadeIn align h-end v-center" on:change={changePlotStyle}>
                 <CustomSelect options={plotStyle} bind:picked={plotStyleSelected} label="Plot Style"/>
                 <CustomCheckbox bind:selected={plotFill} label="Fill area"/>
             </div>
-        
             {/if}
         
         <div id="thzPlot" class="graph__div" ></div>
     
     </svelte:fragment>
+
 </Layout>
