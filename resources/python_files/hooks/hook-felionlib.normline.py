991f560a352cad09a5263da58f4c75d7e51329ab
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-03-11 21:14:29 +0100

    including getfile_details module

--- resources/python_files/hooks/hook-felionlib.normline.py
+++ resources/python_files/hooks/hook-felionlib.normline.py
@@ -2,5 +2,6 @@ hiddenimports = [
     "felionlib.normline.felix", "felionlib.normline.oposcan",
     "felionlib.normline.addTrace", "felionlib.normline.baseline",
     "felionlib.normline.multiGauss", "felionlib.normline.exp_gauss_fit",
-    "felionlib.normline.theory", "felionlib.normline.felix_tkplot"
+    "felionlib.normline.theory", "felionlib.normline.felix_tkplot",
+    "felionlib.normline.getfile_details"
 ]
