1677a3607d90781057a39b7a77cac0bc1111bc9d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-03-12 01:02:40 +0100

    Update main.py

--- resources/python_files/main.py
+++ resources/python_files/main.py
@@ -7,16 +7,8 @@ from importlib import import_module
 if __name__ == "__main__":
 
     startTime = perf_counter()
-    # print(sys.argv[-1])
-
-    # receivedArgs = json.loads(sys.argv[-1])
-    
-    # pyfile = receivedArgs["pyfile"]
-    # args = receivedArgs["args"]
-
     pyfile = sys.argv[1]
     args = json.loads(sys.argv[2])
-
     print(f"{pyfile=}\n{args=}", flush=True)
 
     with warnings.catch_warnings(record=True) as warn:
