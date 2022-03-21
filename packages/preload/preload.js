3aa5cedcd60069487fbc27211a19aa4c0e35f2b2
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-03-21 01:02:28 +0100

    Update preload.js

--- packages/preload/preload.js
+++ packages/preload/preload.js
@@ -1,21 +1,27 @@
 import { ipcRenderer } from 'electron'
+import logger from 'electron-log'
+
+logger.info("preload: loading...")
+
 
 window.addEventListener('contextmenu', (e) => {
     e.preventDefault()
     const rightClickPosition = { x: e.x, y: e.y }
-    // console.log({rightClickPosition})
-    // Renderer to main (one-way)
     ipcRenderer.send("contextmenu", rightClickPosition)
-
 }, false);
+
 
 import "./definedENV"
 import "./update-log"
 import "./mangeServer"
 import "./fs-modules"
-
 import "./path-modules"
 import "./child-process-modules"
 import "./dialogs-modules"
 import "./jsondb-modules"
 import "./utils/logger"
+
+logger.info("preload: loaded")
+window.addEventListener('DOMContentLoaded', () => {
+    logger.info('Preload: DOM fully loaded and parsed');
+});
