4435da9d972f784a491a7d53a71fb0a37f8c869c
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-03-21 01:01:51 +0100

    Update definedEnv.js

--- packages/main/definedEnv.js
+++ packages/main/definedEnv.js
@@ -1,4 +1,4 @@
-import { app } from 'electron'
+// import { app } from 'electron'
 import Store from 'electron-store'
 import path from "path"
 
@@ -13,7 +13,8 @@ try{
 
 } catch (error) {console.error(error)}
 
-export const { isPackaged } = app
-export const ROOT_DIR = isPackaged ? path.dirname(app.getPath("module")) : path.join(__dirname, "../../../")
+// export const { isPackaged } = app
+// export const ROOT_DIR = isPackaged ? path.dirname(app.getPath("module")) : path.join(__dirname, "../../../")
+export const ROOT_DIR = path.join(__dirname, "../../../")
 export const PKG_DIR = path.join(ROOT_DIR, "packages")
 export const RENDERER_DIR = path.join(PKG_DIR, "renderer")
