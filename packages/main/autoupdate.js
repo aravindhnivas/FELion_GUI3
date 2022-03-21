d241c798bae9f502df21b31ce540f6d465cdbcae
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-03-21 01:02:02 +0100

    Update autoupdate.js

--- packages/main/autoupdate.js
+++ packages/main/autoupdate.js
@@ -12,15 +12,25 @@ autoUpdater.logger.transports.file.level = 'info';
 
 
 
-if(app.isPackaged) { autoUpdater.checkForUpdates() }
+if(app.isPackaged) { 
+    try {
+        autoUpdater.checkForUpdates()
+    } catch (error) {
+        logger.error(error)
+    }
+ }
 logger.info('App starting...');
 
 ipcMain.handle("checkupdate", () => {autoUpdater.checkForUpdates()})
 const updateLog = (info) => {logger.info(info); mainWindow.webContents.send("update-log", info)}
 
 autoUpdater.on('checking-for-update', () => updateLog("checking-for-update" + '\n-----------\n'))
-autoUpdater.on('update-available', (info) => {updateLog('update-available: \n'+ JSON.stringify(info) + '\n-----------\n');})
-autoUpdater.on('update-not-available', (info) => {updateLog('update-not-available ' + JSON.stringify(info) + '\n-----------\n');})
+autoUpdater.on('update-available', (info) => {
+    updateLog('update-available: \n'+ JSON.stringify(info) + '\n-----------\n');
+})
+autoUpdater.on('update-not-available', (info) => {
+    updateLog('update-not-available ' + JSON.stringify(info) + '\n-----------\n');
+})
 autoUpdater.on('error', (err) => {logger.error(err); mainWindow.webContents.send("update-log-error", err)})
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
