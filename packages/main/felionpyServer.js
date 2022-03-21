4c3f7ad24b8988b1d464e08d27b59eda279bcc01
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-03-21 01:31:15 +0100

    Update felionpyServer.js

--- packages/main/felionpyServer.js
+++ packages/main/felionpyServer.js
@@ -1,4 +1,4 @@
-import { ipcMain } from 'electron'
+import { app, ipcMain } from 'electron'
 import path from "path"
 import { promisify } from 'util'
 import { spawn, exec  } from 'child_process'
@@ -13,19 +13,26 @@ const getCurrentDevStatus = () => {
 
     const db = new Store({name: "db"})
     
-    if(!db.has("developerMode")) {
-        db.set("developerMode", env.DEV)
+    if(!db.has("developerMode") || env.PROD) {
+        db.set("developerMode", false)
     }
+
     const developerMode = db.get("developerMode")
     const pythonscript = db.get("pythonscript") || path.join(ROOT_DIR, "resources/python_files")
     const pythonpath = db.get("pythonpath") || path.join(ROOT_DIR, "resources/python_files")
-    const pyProgram = developerMode ? pythonpath : path.join(ROOT_DIR, "resources/felionpy/felionpy")
+    let pyProgram;
+    if (app.isPackaged) {
+        pyProgram = path.join(ROOT_DIR, "../../resources/felionpy/felionpy")
+    } else {
+        pyProgram = developerMode ? pythonpath : path.join(ROOT_DIR, "resources/felionpy/felionpy")
+    }
+
     const mainpyfile = developerMode ? path.join(pythonscript, "main.py") : ""
 
     return {db, developerMode, pyProgram, mainpyfile}
 
 }
-let pyVersion;
+let pyVersion="";
 
 export async function getPyVersion() {
     
@@ -35,6 +42,8 @@ export async function getPyVersion() {
         const pyfile = "getVersion"
 
         const command = `${pyProgram} ${mainpyfile} ${pyfile} {} `
+
+        console.info(command)
         const {stdout} = await execCommand(command)
 
         const [version] = stdout?.split("\n")?.filter?.(line => line.includes("Python")) || [""]
@@ -72,7 +81,6 @@ export async function startServer(webContents) {
 
     
         webContents?.send('db:update', {key: "pyServerReady", value: false})
-        // pyVersion ||= db.get("pyVersion")
         webContents?.send('db:update', {key: "pyVersion", value: pyVersion})
 
         if(!pyVersion) {
