a9611157bf6eefdb7c54430c7fbf4dd06690bde7
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-03-28 18:05:56 +0200

    Update workers.py

--- resources/python_files/felionlib/utils/felionQt/utils/workers.py
+++ resources/python_files/felionlib/utils/felionQt/utils/workers.py
@@ -1,6 +1,7 @@
 
-import traceback, sys
+import sys
 from PyQt6.QtCore import pyqtSignal, pyqtSlot, QObject, QRunnable
+
 
 class WorkerSignals(QObject):
     '''
@@ -42,9 +43,10 @@ class Worker(QRunnable):
     '''
 
     def __init__(self, fn, *args, **kwargs):
-        super(Worker, self).__init__()
 
+        super(Worker, self).__init__()
         # Store constructor arguments (re-used for processing)
+        
         self.fn = fn
         self.args = args
         self.kwargs = kwargs
@@ -59,15 +61,13 @@ class Worker(QRunnable):
         '''
         Initialise the runner function with passed args, kwargs.
         '''
-
         # Retrieve args/kwargs here; and fire processing using them
         try:
             result = self.fn(*self.args, **self.kwargs)
+
         except:
-            
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            self.signals.error.emit((exctype, value, traceback.format_exc()))
+            etype, value, tb = sys.exc_info()
+            self.signals.error.emit((etype, value, tb))
         else:
             self.signals.result.emit(result)  # Return the result of the processing
         finally:
