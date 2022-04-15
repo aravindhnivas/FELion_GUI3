e8ca1f1c329eb621e4aefbd2d820f006a634f6f5
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-04-15 23:55:02 +0200

    Update CustomIconSwitch.svelte

--- packages/renderer/src/components/CustomIconSwitch.svelte
+++ packages/renderer/src/components/CustomIconSwitch.svelte
@@ -1,12 +1,11 @@
-
 <script>
     import IconButton, {Icon} from '@smui/icon-button'
-    export let icons = [], toggler="", style=""
+    export let toggler=""
+    export let style=""
+    export let icons = []
 </script>
 
 <IconButton {style} toggle bind:pressed={toggler} on:click>
-
     <Icon class="material-icons" on>{icons[0]}</Icon>
-
     <Icon class="material-icons">{icons[1]}</Icon>
 </IconButton>
