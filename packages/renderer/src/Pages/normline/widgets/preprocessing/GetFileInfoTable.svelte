b8a4b25ab8d5cd45d67c110550df612a6f99d718
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-04-15 23:54:49 +0200

    Update GetFileInfoTable.svelte

--- packages/renderer/src/Pages/normline/widgets/preprocessing/GetFileInfoTable.svelte
+++ packages/renderer/src/Pages/normline/widgets/preprocessing/GetFileInfoTable.svelte
@@ -1,14 +1,16 @@
 
 <script>
-    import { filedetails, opoMode, normMethod }         from "../../functions/svelteWritables";
-    import CustomIconSwitch                 from '$components/CustomIconSwitch.svelte';
-    import Table                            from '$components/Table.svelte';
-    import { savefile,  loadfile }          from '../../functions/misc';
-    import {uniqBy}                         from "lodash-es"
-    import computePy_func                   from "$src/Pages/general/computePy"
-    import {get_details_func}               from '../../functions/get_details';
+
+    import { filedetails, opoMode, normMethod } from "../../functions/svelteWritables";
+    import CustomIconSwitch                     from '$components/CustomIconSwitch.svelte';
+    import Table                                from '$components/Table.svelte';
+    import { savefile,  loadfile }              from '../../functions/misc';
+    import {uniqBy}                             from "lodash-es"
+    import computePy_func                       from "$src/Pages/general/computePy"
+    import {get_details_func}                   from '../../functions/get_details';
 
     export let felixfiles
+    
     let toggleFileDetailsTable = false
     
     async function plotData({e=null}={}){
@@ -16,29 +18,41 @@
         if(felixfiles.length<1) return window.createToast("No files selected", "danger")
         
         const pyfile="normline.getfile_details"
-        const args = {files:$opoMode?opofiles : felixfiles, normMethod: $normMethod}
+        const files = $opoMode ? opofiles : felixfiles
+        const args = {files, normMethod: $normMethod}
         
         const dataFromPython = await computePy_func({e, pyfile, args})
         if(!dataFromPython) return
+
         get_details_func({dataFromPython})
         toggleFileDetailsTable = true
+
     }
 
     function loadfiledetails(){
-        const loadedfile = loadfile({name:"filedetails"})
+        const loadedfile = loadfile("filedetails")
         $filedetails = uniqBy([...loadedfile, ...$filedetails], "filename")
     }
-
 </script>
 
-<div class="align"> 
+<div class="align">
+
     <button class="button is-link" on:click="{(e)=>plotData({e:e})}">Get details</button>
     <CustomIconSwitch bind:toggler={toggleFileDetailsTable} icons={["arrow_drop_down", "arrow_drop_up"]}/>
     <button class="button is-link" on:click="{()=>savefile({file:$filedetails, name:"filedetails"})}">Save</button>
     <button class="button is-link" on:click="{loadfiledetails}">Load</button>
     
     {#if toggleFileDetailsTable}
-        <Table head={["Filename", "min(cm-1)", "max(cm-1)", "Trap(s)", "B0(ms)", "Res.(V)", "IE(eV)", "Temp(K)","Precursor", ]} bind:rows={$filedetails} keys={["filename", "min", "max", "trap", "b0", "res", "ie","temp", "precursor"]} id="felix_filedetails_table" closeOption={false} sortOption={true}/>
+        <Table 
+            id="felix_filedetails_table" bind:rows={$filedetails}
+            closeOption={false} sortOption={true} animateRow={false}
+            
+            head={[
+                "Filename", "min(cm-1)", "max(cm-1)", "Trap(s)", 
+                "B0(ms)", "Res.(V)", "IE(eV)", "Temp(K)", "Precursor"
+            ]}
+            keys={[ "filename", "min", "max", "trap", "b0", "res", "ie","temp", "precursor" ]} 
+         />
 
     {/if}
 
