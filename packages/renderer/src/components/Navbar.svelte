7ffa3b8bb7a5337263c68a62b7be18f4cc862e63
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-04-16 00:34:40 +0200

    Update Navbar.svelte

--- packages/renderer/src/components/Navbar.svelte
+++ packages/renderer/src/components/Navbar.svelte
@@ -4,16 +4,14 @@
 	import TabBar       from '@smui/tab-bar';
 	import {onMount}    from "svelte";
   	import {activePage} from '$src/sveltewritable';
+
 	export let navItems;
-	
 	
 	let active = db.get('active_tab') || 'Home';
 	$: $activePage = active
 	$: console.log(`Current page: ${$activePage}`)
 
 	$: db.set('active_tab', $activePage)
-
-
 	const navigate = () =>{navItems.forEach(
 		item => item == active 
 		? document.getElementById(item).style.display = "block" 
@@ -28,7 +26,6 @@
 		<Tab {tab}> <Label>{tab}</Label> </Tab>
 	</TabBar>
 </div>
-
 
 <style lang="scss">
 	#navbar { width: 100vw; margin-bottom: 0; padding: 0; }
