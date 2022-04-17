1a9f2db1a02a909ec7fc8061463bba1cf6b3f1c6
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-04-17 19:57:05 +0200

    Update autoupdate.js

--- packages/main/autoupdate.js
+++ packages/main/autoupdate.js
@@ -5,7 +5,7 @@ import logger from 'electron-log'
 
 const mainWindow = BrowserWindow.getAllWindows()[0]
 
-const {showMessageBoxSync} = dialog;
+// const {showMessageBox} = dialog;
 
 autoUpdater.logger = logger;
 autoUpdater.logger.transports.file.level = 'info';
@@ -13,12 +13,12 @@ autoUpdater.logger.transports.file.level = 'info';
 
 
 if(app.isPackaged) { 
-    try {
-        autoUpdater.checkForUpdates()
-    } catch (error) {
-        logger.error(error)
-    }
- }
+	try {
+		autoUpdater.checkForUpdates()
+	} catch (error) {
+		logger.error(error)
+	}
+}
 logger.info('App starting...');
 
 ipcMain.handle("checkupdate", () => {autoUpdater.checkForUpdates()})
@@ -26,29 +26,27 @@ const updateLog = (info) => {logger.info(info); mainWindow.webContents.send("upd
 
 autoUpdater.on('checking-for-update', () => updateLog("checking-for-update" + '\n-----------\n'))
 autoUpdater.on('update-available', (info) => {
-    updateLog('update-available: \n'+ JSON.stringify(info) + '\n-----------\n');
+	updateLog('update-available: \n'+ JSON.stringify(info) + '\n-----------\n');
 })
 autoUpdater.on('update-not-available', (info) => {
-    updateLog('update-not-available ' + JSON.stringify(info) + '\n-----------\n');
+	updateLog('update-not-available ' + JSON.stringify(info) + '\n-----------\n');
 })
 autoUpdater.on('error', (err) => {logger.error(err); mainWindow.webContents.send("update-log-error", err)})
 autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  updateLog('download-progress: ' + log_message + '\n-----------\n')
-  mainWindow.webContents.send("update-progress", progressObj)
+	let log_message = "Download speed: " + progressObj.bytesPerSecond;
+	log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+	log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+	updateLog('download-progress: ' + log_message + '\n-----------\n')
+	mainWindow.webContents.send("update-progress", progressObj)
 })
 
 autoUpdater.on('update-downloaded', async (info) => {
-    logger.info('update-downloaded' + info);
-
-    const restartArgs = { 
-        title: "FELion_GUI3", type: "info", message: "Update donwloaded",
-        buttons: ["Restart and Install", "Later"]
-     }
-    const response = await showMessageBoxSync(mainWindow, restartArgs)
-    if(response === 0) {
-        autoUpdater.quitAndInstall();
-    }
+	logger.info('update-downloaded' + info);
+	const restartArgs = { 
+		title: "FELion_GUI3", type: "info", message: "Update donwloaded",
+		buttons: ["Restart and Install", "Later"]
+	}
+	
+	const message = await dialog.showMessageBox(mainWindow, restartArgs)
+	if(message.response === 0) {autoUpdater.quitAndInstall()}
 })
