84cdfc89a99aafc838a7cf3a5839f2ca94679761
aravindhnivas <aravindhnivas28@gmail.com>
2022-04-20 16:10:51 +0200

    code signing using custom pfx certificate file

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const certificate = path.join(__dirname, "certificate");  // path to the certificate
+const certificateFile = path.resolve(certificate, 'felion.pfx')  // path to the private key
+
 /**
  * @type {import('electron-builder').Configuration}
  * @see https://www.electron.build/configuration/configuration
@@ -6,11 +10,12 @@ const config = {
   directories: {
     output: 'dist',
     buildResources: 'buildResources',
+
   },
+
   files: [
     'packages/**/dist/**',
   ],
-  
   extraFiles: [ // copy to ROOT_DIR (or Contents in mac)
     'resources'
   ],
@@ -24,11 +29,14 @@ const config = {
   ],
   win: {
     target: "nsis",
-    asar: true
+    asar: true,
+    certificateFile: certificateFile,
+    certificatePassword: "felion",
+    publisherName: ["A.N.Marimuthu"],
+    signingHashAlgorithms: ["sha256"],
   },
   nsis: {
     oneClick: true
   }
 };
 module.exports = config;
-
