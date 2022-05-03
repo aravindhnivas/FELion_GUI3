abae2de1750c709fe79efa0cc656f0334ad2690b
aravindhnivas <aravindhnivas28@gmail.com>
2022-05-03 11:26:22 +0200

    Update hook-felionlib.py

--- resources/python_files/hooks/hook-felionlib.py
+++ resources/python_files/hooks/hook-felionlib.py
@@ -1,5 +1,11 @@
+
+import felionlib
+from pathlib import Path as pt
+
 hiddenimports = [
     "felionlib.getVersion", "felionlib.depletionscan", "felionlib.timescan",
     "felionlib.thz_scan", "felionlib.utils", "felionlib.mass", "felionlib.server",
     "felionlib.kineticsCode", "felionlib.normline", "felionlib.ROSAA"
 ]
+icons_path = pt(felionlib.__file__).parent / "../icons/*"
+datas = [(icons_path.resolve().__str__(), "icons")]
