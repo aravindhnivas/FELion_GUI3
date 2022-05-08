e7f92c873c20bd084e4c96a5ead07c5e3e175fe7
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-05-08 20:50:25 +0200

    Update mangeServer.js

--- packages/preload/mangeServer.js
+++ packages/preload/mangeServer.js
@@ -1,7 +1,9 @@
 import { contextBridge, ipcRenderer } from 'electron'
 contextBridge.exposeInMainWorld('startServer', async () => {
+    console.warn("starting server")
     return ipcRenderer.invoke('startServer')
 })
 contextBridge.exposeInMainWorld('stopServer', () => {
+    console.warn("invoking stopServer")
     ipcRenderer.send('stopServer')
 })
