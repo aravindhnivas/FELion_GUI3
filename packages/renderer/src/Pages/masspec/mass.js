72861d26ca06be0095ce15f447e8a6625827638e
aravindhnivas <aravindhnivas28@gmail.com>
2022-05-09 19:24:26 +0200

    Update mass.js

--- packages/renderer/src/Pages/masspec/mass.js
+++ packages/renderer/src/Pages/masspec/mass.js
@@ -1,13 +1,15 @@
-import { getInfoContents } from '../computeCode/utils'
-
+// import { getInfoContents } from '../computeCode/utils'
+import get_files_settings_values from '$src/js/get_files_settings_values'
 export async function readMassFile(massfiles) {
+    const loadbtn = document.getElementById('masspec-plot-btn')
+    loadbtn.classList.toggle('is-loading')
     try {
         const dataToSend = {}
 
         for (const filename of massfiles) {
             if (!fs.existsSync(filename)) return
             console.log(filename)
-            const fileContents = fs.readFileSync(filename)
+            const [fileContents] = await fs.readFile(filename)
 
             const name = basename(filename)
             console.info('content read: ', name)
@@ -29,23 +31,29 @@ export async function readMassFile(massfiles) {
             const showlegend = true
             console.info(name, 'done\n')
 
-            const fileVariableComputedValues = getInfoContents(fileContents)
-
+            // const fileVariableComputedValues = getInfoContents(fileContents)
+            const fileVariableComputedValues = await get_files_settings_values(filename)
+            // console.info(fileVariableComputedValues, 'computed')
             const res = fileVariableComputedValues['m03_ao13_reso']
             const b0 = fileVariableComputedValues['m03_ao09_width'] / 1000
             const trap = fileVariableComputedValues['m04_ao04_sa_delay'] / 1000
 
-            const label = `${name}: Res:${res.toFixed(1)} V; B0: ${b0.toFixed(
+            const label = `${name}: Res:${res?.toFixed(1)} V; B0: ${b0?.toFixed(
                 0
-            )} ms; trap: ${trap.toFixed(0)} ms`
+            )} ms; trap: ${trap?.toFixed(0)} ms`
             dataToSend[name] = { x, y, name: label, mode, showlegend }
         }
 
         console.info('File read completed')
         console.info(dataToSend)
+
         return [dataToSend, null]
+
     } catch (error) {
         window.handleError(error)
         return [null, error]
+    } finally {
+        loadbtn.classList.toggle('is-loading')
     }
+
 }
