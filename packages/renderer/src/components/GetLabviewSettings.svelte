8fdde206626fa5ff09c3b031d1951e1f4b7eff59
aravindhnivas <aravindhnivas28@gmail.com>
2022-05-09 17:14:13 +0200

    get labview settings update

--- packages/renderer/src/components/GetLabviewSettings.svelte
+++ packages/renderer/src/components/GetLabviewSettings.svelte
@@ -1,126 +1,48 @@
 <script>
     import Modal from './Modal.svelte'
-    import { fade } from 'svelte/transition'
-    import { onMount, afterUpdate } from 'svelte'
     import Textfield from '@smui/textfield'
     import CustomSelect from './CustomSelect.svelte'
-
     import CustomSwitch from './CustomSwitch.svelte'
-    export let currentLocation, fullfileslist, fileChecked
+    import settings_key_value_infos from '$src/settings_key_value_infos.json'
+
+    export let currentLocation = ''
+    export let fullfileslist = []
+    export let fileChecked = []
     export let active = false
 
-    // const settingsVariable = {
-
-    //     b0: ['m03_ao09_bl0', 'm03_ao09_high', 'm03_ao09_width'],
-    //     bq0: ['m03_ao01_bq0'],
-    //     bq5: ['m03_ao03_bq5'],
-    //     q1float: ['m03_ao15_qd1_float'],
-    //     q2float: ['m04_ao09_qd2_float'],
-    //     res: ['m03_ao13_reso'],
-    //     bqLenses: [
-    //         'm03_ao08_bq1',
-    //         'm03_ao07_bq2',
-    //         'm03_ao06_bq3',
-    //         'm03_ao05_bq4',
-    //     ],
-    //     benderLenses: ['m04_ao01_b_in', 'm04_ao00_b_outer', 'm04_ao02_b_inner'],
-    //     se: [
-    //         'm04_ao03_se_trap_in',
-    //         'm04_ao03_se_delay',
-    //         'm04_ao03_se_high',
-    //         'm04_ao03_se_width',
-    //     ],
-    //     sa: [
-    //         'm04_ao04_sa_trap_out',
-    //         'm04_ao04_sa_delay',
-    //         'm04_ao04_sa_high',
-    //         'm04_ao04_sa_width',
-    //     ],
-    //     trapfloat: ['m04_ao05_trap_float'],
-    //     bl4: ['m04_ao07_bl4'],
-    //     bl5: ['m04_ao08_bl5'],
-    // }
-
-    const settingsVariable = {
-        "B0": ['m03_ao09_bl0', 'm03_ao09_high', 'm03_ao09_width'],
-        "Quad float": ['m03_ao15_qd1_float', 'm04_ao09_qd2_float'],
-        "RES": ['m03_ao13_reso'],
-        "bq lenses": [
-            'm03_ao01_bq0',
-            'm03_ao08_bq1',
-            'm03_ao07_bq2',
-            'm03_ao06_bq3',
-            'm03_ao05_bq4',
-            'm03_ao03_bq5'
-        ],
-        "bender lenses": [
-            'm04_ao01_b_in',
-            'm04_ao00_b_outer',
-            'm04_ao02_b_inner'
-        ],
-        "SE": [
-            'm04_ao03_se_trap_in',
-            'm04_ao03_se_delay',
-            'm04_ao03_se_high',
-            'm04_ao03_se_width',
-        ],
-        "SA": [
-            'm04_ao04_sa_trap_out',
-            'm04_ao04_sa_delay',
-            'm04_ao04_sa_high',
-            'm04_ao04_sa_width',
-        ],
-        "trap float": ['m04_ao05_trap_float'],
-        "bl lenses": ['m04_ao07_bl4', 'm04_ao08_bl5'],
-    }
-
     const style = 'width:14em; height:3.5em; margin-right:0.5em'
-    let fileContents = ''
-
-    let variableValues = {}
-    let settingsLoaded = false
-
-    const update_settings_values = () => {
-        const fullFilename = pathJoin(currentLocation, selected_file)
-        console.log(fullFilename)
-        settingsLoaded = false
-
-        if (fs.existsSync(fullFilename) && selected_file) {
-            fileContents = fs.readFileSync(fullFilename)
-
-            for (const line of fileContents.split('\n')) {
-                if (
-                    line.trim().length > 0 &&
-                    line.startsWith('# Sect01 Ion Source')
-                ) {
-                    let tempLine = line.split(' ')
-                    variableValues[tempLine[7]] = parseFloat(tempLine[9])
-                }
-            }
-            settingsLoaded = true
-        }
+    const validate_line = (line) => {
+        const valid =
+            line.trim().length > 0 && line.startsWith('# Sect01 Ion Source')
+        return valid
     }
-    // afterUpdate()
+
+    const get_settings_value = (selected_file) => {
+        return new Promise(async (resolve, reject) => {
+            if (!selected_file) return reject('No file selected')
+
+            const filename = pathJoin(currentLocation, selected_file)
+            if (!fs.existsSync(filename))
+                return reject(`${filename} does not exist`)
+
+            const [fileContents, error] = await fs.readFile(filename)
+            if (error) return reject(error)
+
+            const variableValues = {}
+            for (const line of fileContents.split('\n')) {
+                if (!validate_line(line)) continue
+                const keyValuesLine = line.split(' ')
+                const key = keyValuesLine[7]
+                const value = keyValuesLine[9]
+                variableValues[key] = value
+            }
+            return resolve(variableValues)
+        })
+    }
+
     let showAllFiles = true
-
-    $: displayFiles = showAllFiles ? fullfileslist : fileChecked
-
-    // const labelRowB0 = ['B0 Low', 'B0 high', 'B0 Width']
-
-    // const labelRowSE = ['Trap-in', 'SE delay', 'SE high', 'SE Width']
-    // const labelRowSA = ['Trap-out', 'Trap time (ms)', 'SA high', 'SA Width']
-
-    // const bqlensLabel = ['bq1', 'bq2', 'bq3', 'bq4']
-    // const benderLabel = ['Bender in', 'Bender outer', 'Bender inner']
     let selected_file = ''
-    $: if(selected_file) {update_settings_values()}
-    onMount(()=>{
-
-        if(displayFiles.length > 0) {
-            selected_file = displayFiles[0]
-        }
-    })
-
+    $: displayFiles = showAllFiles ? fullfileslist : fileChecked
 </script>
 
 <button
@@ -132,172 +54,67 @@
 
 {#if active}
     <Modal title="Labview Settings" bind:active>
-        <div slot="content" transition:fade>
-            <CustomSwitch
-                style="margin: 0 1em;"
-                bind:selected={showAllFiles}
-                label="show all"
-            />
-            <CustomSelect
-                bind:picked={selected_file}
-                label="Filename"
-                options={displayFiles}
-            />
+        <svelte:fragment slot="body_header__div">
+            <div class="controller">
+                <CustomSwitch bind:selected={showAllFiles} label="show all" />
+                <CustomSelect
+                    auto_init={true}
+                    bind:picked={selected_file}
+                    label="Filename"
+                    options={displayFiles}
+                />
+            </div>
+        </svelte:fragment>
 
-            {#if settingsLoaded}
-                <!-- <div class="container">
-                    <div class="b0 col">
-                        {#each settingsVariable.b0 as item, index (item)}
-                            <Textfield
-                                {style}
-                                value={variableValues[item]}
-                                label={`${labelRowB0[index]} (${item})`}
-                            />
-                        {/each}
-                    </div>
-
-                    <div class="row">
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.q1float]}
-                            label="Quad 1 float ({settingsVariable.q1float})"
-                        />
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.bq0]}
-                            label="bq0 ({settingsVariable.bq0})"
-                        />
-                    </div>
-                    <div class="bqLenses col">
-                        {#each settingsVariable.bqLenses as item, index (item)}
-                            <Textfield
-                                {style}
-                                value={variableValues[item]}
-                                label={`${bqlensLabel[index]} (${item})`}
-                            />
-                        {/each}
-                    </div>
-
-                    <div class="row">
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.bq5]}
-                            label="bq5 ({settingsVariable.bq5})"
-                        />
-                    </div>
-                    <div class="benderLenses col">
-                        {#each settingsVariable.benderLenses as item, index (item)}
-                            <Textfield
-                                {style}
-                                value={variableValues[item]}
-                                label={`${benderLabel[index]} (${item})`}
-                            />
-                        {/each}
-                    </div>
-
-                    <div class="row">
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.bl4]}
-                            label="bl4 ({settingsVariable.bl4})"
-                        />
-                    </div>
-                    <div class="se row">
-                        {#each settingsVariable.se as item, index (item)}
-                            <Textfield
-                                {style}
-                                value={variableValues[item]}
-                                label={`${labelRowSE[index]} (${item})`}
-                            />
-                        {/each}
-                    </div>
-
-                    <div class="row">
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.trapfloat]}
-                            label="trapfloat ({settingsVariable.trapfloat})"
-                        />
-                    </div>
-
-                    <div class="sa row">
-                        {#each settingsVariable.sa as item, index (item)}
-                            <Textfield
-                                {style}
-                                value={variableValues[item]}
-                                label={`${labelRowSA[index]} (${item})`}
-                            />
-                        {/each}
-                    </div>
-
-                    <div class="row" style="padding: 1em 0;">
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.bl5]}
-                            label="bl5 ({settingsVariable.bl5})"
-                        />
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.res]}
-                            label="Resolution ({settingsVariable.res})"
-                        />
-
-                        <Textfield
-                            {style}
-                            value={variableValues[settingsVariable.q2float]}
-                            label="Quad 2 float ({settingsVariable.q2float})"
-                        />
-                    </div>
-                </div> -->
-
-
-                {#if variableValues}
-                    <div class="container">
-                        {#each Object.keys(settingsVariable) as key (key)}
-                            <div class="row">
-                                <h1>{key}</h1>
-                                <div class="row-contents">
-                                    {#each settingsVariable[key] as label}
-                                        <Textfield
-                                            {style}
-                                            value={variableValues[label] ?? ''}
-                                            {label}
-                                        />
-                                    {/each}
-                                </div>
+        <svelte:fragment slot="body_scrollable__div">
+            {#await get_settings_value(selected_file)}
+                <div class="info-box">loading...</div>
+            {:then variableValues}
+                <div class="container">
+                    {#each Object.keys(settings_key_value_infos) as id (id)}
+                        <div class="row">
+                            <h1>{id}</h1>
+                            <div class="row-contents">
+                                {#each settings_key_value_infos[id] as key}
+                                    <Textfield
+                                        {style}
+                                        value={variableValues[key] ?? ''}
+                                        label={key}
+                                        disabled
+                                    />
+                                {/each}
                             </div>
-                        {/each}
-                    </div>
-
-                {/if}
-
-            {/if}
-        </div>
+                        </div>
+                    {/each}
+                </div>
+            {:catch error}
+                <div class="info-box error">{error}</div>
+            {/await}
+        </svelte:fragment>
     </Modal>
 {/if}
 
 <style>
-    .row {
+    .info-box {
         display: flex;
-        flex-wrap: wrap;
+        margin-top: 1em;
         justify-content: center;
+        padding: 1em;
     }
-    .row,
-    .col {
+    .error {
+        background: #f14668;
+    }
+    .controller {
+        display: flex;
+        gap: 1em;
+    }
+    .row {
         margin-bottom: 2em;
-    }
-    .col {
-        display: grid;
     }
     .container {
         display: grid;
-        place-items: center;
-        /* margin: 1em 6em; */
-        /* overflow-y: auto; */
-        /* max-height: 30vh; */
         padding: 1em;
-        /* border: solid 1px; */
-        /* border-radius: 1em; */
-        /* justify-content: center; */
+        height: 90%;
+        overflow-y: auto;
     }
 </style>
