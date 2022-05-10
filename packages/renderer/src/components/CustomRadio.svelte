c62f57f6d56a1b7ddb9827c7ee6a96d0563e6136
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-05-10 22:33:42 +0200

    Update CustomRadio.svelte

--- packages/renderer/src/components/CustomRadio.svelte
+++ packages/renderer/src/components/CustomRadio.svelte
@@ -3,13 +3,13 @@
     import FormField from '@smui/form-field'
 
     export let options = []
-    export let selected = ''
+    export let value = ''
     export let style = ''
 </script>
 
 {#each options as option}
     <FormField {style}>
-        <Radio on:change bind:group={selected} value={option} />
+        <Radio on:change bind:group={value} value={option} />
         <span slot="label">{option}</span>
     </FormField>
 {/each}
