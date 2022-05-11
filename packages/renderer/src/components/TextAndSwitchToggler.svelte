27349a199b6b01e0bc167585f168139e2591d7a6
aravindhnivas <aravindhnivas28@gmail.com>
2022-05-11 18:19:20 +0200

    Create TextAndSwitchToggler.svelte

---
+++ packages/renderer/src/components/TextAndSwitchToggler.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Textfield from '@smui/textfield'
+    import CustomSelect from './CustomSelect.svelte'
+
+    export let value = ''
+    export let label = ''
+    export let options = []
+    export let update = () => {}
+
+    let toggle = true
+    console.log('toggle', toggle)
+</script>
+
+<svelte:component
+    this={toggle ? CustomSelect : Textfield}
+    on:click={(e) => {
+        if (e.ctrlKey) toggle = !toggle
+    }}
+    bind:value
+    {label}
+    {update}
+    options={[value, ...options.filter((file) => file !== value)]}
+/>
