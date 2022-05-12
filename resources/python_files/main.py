6fcebbc9a84f7b67e97f95bd93f85be90f53dcbb
aravindhnivas <aravindhnivas28@gmail.com>
2022-05-12 11:45:37 +0200

    Update main.py

--- resources/python_files/main.py
+++ resources/python_files/main.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         args = json.loads(sys.argv[2])
 
+    print(f"{pyfile=}\n", flush=True)
     if "verbose" in args and args["verbose"]:
-        print(f"{pyfile=}\n{args=}", flush=True)
+        print(f"{args=}", flush=True)
 
     with warnings.catch_warnings(record=True) as warn:
         pyfunction = import_module(f"felionlib.{pyfile}")
