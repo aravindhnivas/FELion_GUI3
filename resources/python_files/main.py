9ab737cde009314de9b09e1d326152e4cda1a052
aravindhnivas <aravindhnivas28@gmail.com>
2022-05-12 11:43:46 +0200

    optional verbose in main felionpy

--- resources/python_files/main.py
+++ resources/python_files/main.py
@@ -2,32 +2,21 @@ import sys
 import json
 import warnings
 from importlib import import_module
-# import traceback
-# import ctypes
+
 
 if __name__ == "__main__":
 
     pyfile = sys.argv[1]
     args = None
-    if len(sys.argv)>2:
+    if len(sys.argv) > 2:
         args = json.loads(sys.argv[2])
 
-    print(f"{pyfile=}\n{args=}", flush=True)
+    if "verbose" in args and args["verbose"]:
+        print(f"{pyfile=}\n{args=}", flush=True)
+
     with warnings.catch_warnings(record=True) as warn:
         pyfunction = import_module(f"felionlib.{pyfile}")
-        
         if args:
             pyfunction.main(args)
         else:
             pyfunction.main()
-
-# def excepthook(etype, value, tb):
-#     tb = "".join(traceback.format_exception(etype, value, tb, limit=5))
-#     print("error catched!:", value, flush=True)
-#     print("error message:\n", tb, flush=True)
-    
-#     MB_ICONERROR = 0x10
-#     ctypes.windll.user32.MessageBoxW(None, tb, etype.__name__, MB_ICONERROR)
-#     return
-    
-# sys.excepthook = excepthook
