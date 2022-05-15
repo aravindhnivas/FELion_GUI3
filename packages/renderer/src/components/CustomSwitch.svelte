c6f9abfd076f6451ce1dfaff2d56ea0baa5844d4
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-05-15 17:42:48 +0200

    Update CustomSwitch.svelte
    tooltip

--- packages/renderer/src/components/CustomSwitch.svelte
+++ packages/renderer/src/components/CustomSwitch.svelte
@@ -4,9 +4,19 @@
     export let selected = false
     export let style = ''
     export let label = ''
+    export let tooltip = ''
+    export let tooltipDir = 'bottom'
 </script>
 
 <FormField {style}>
     <Switch on:SMUISwitch:change bind:checked={selected} />
-    <span slot="label">{label}</span>
+    <svelte:fragment slot="label">
+        {#if tooltip}
+            <span aria-label={tooltip} data-cooltipz-dir={tooltipDir}
+                >{label}</span
+            >
+        {:else}
+            <span>{label}</span>
+        {/if}
+    </svelte:fragment>
 </FormField>
