f7c63392a657af84e3640fe6fca2a37bb93a0368
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-05-15 22:31:37 +0200

    Create Modal.svelte

---
+++ packages/renderer/src/components/modal/Modal.svelte
@@ -0,0 +1,43 @@
+<script>
+    import Dialog, { Title, Content, Actions } from '@smui/dialog'
+    import { createEventDispatcher, onMount } from 'svelte'
+
+    export let id = getID()
+    export let open = false
+    export let title = ''
+    let className = ''
+    export { className as class }
+    export let content$style = ''
+
+    const dispatch = createEventDispatcher()
+
+    onMount(() => {
+        dispatch('mounted')
+        return () => dispatch('destroyed')
+    })
+</script>
+
+<Dialog
+    bind:open
+    scrimClickAction=""
+    surface$class="background-body {className}"
+    aria-labelledby="{id}-title"
+    aria-describedby="{id}-content"
+    surface$style="min-width: 30vw; min-height: 30vh;  max-width:60vw; max-height: 70vh; background-color: #5a419b;"
+    container$style={content$style}
+    on:SMUIDialog:closed={(e) => dispatch('closed', { event: e })}
+>
+    <Title style="color: white; background: #836ac05c;" id="{id}-title"
+        >{title}</Title
+    >
+    <Content id="{id}-content">
+        <slot name="content" />
+    </Content>
+    <Actions style="background: #836ac05c;">
+        <slot name="footer">
+            <button class="button is-danger" on:click={() => (open = false)}
+                >Close</button
+            >
+        </slot>
+    </Actions>
+</Dialog>
