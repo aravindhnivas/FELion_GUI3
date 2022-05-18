445e7fed698b952fa7a4bf523b38b7cc0fcc88ff
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-05-18 14:52:57 +0200

    Update Modal.svelte

--- packages/renderer/src/components/modal/Modal.svelte
+++ packages/renderer/src/components/modal/Modal.svelte
@@ -24,19 +24,12 @@
     aria-labelledby="{id}-title"
     aria-describedby="{id}-content"
     surface$style="min-width: 30vw; min-height: 30vh;  max-width:60vw; max-height: 70vh; background-color: #5a419b;"
-    container$style={content$style}
     on:SMUIDialog:closed={(e) => dispatch('closed', { event: e })}
 >
     <Title style="color: white; background: #836ac05c;" id="{id}-title"
         >{title}</Title
     >
-    <Content id="{id}-content">
-        <!-- <div class="container">
-            <h2 class="header">Header</h2>
-            <div class="content">
-                <slot name="content" />
-            </div>
-        </div> -->
+    <Content id="{id}-content" style={content$style}>
         <slot name="content" />
     </Content>
     <Actions style="background: #836ac05c;">
