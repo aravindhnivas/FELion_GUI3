7fa90fa4fb33d10e0d98af2df7ef61e4a0a40df9
aravindhnivas <aravindhnivas28@gmail.com>
2022-05-25 17:23:31 +0200

    adding gridjs

--- packages/renderer/src/App.scss.js
+++ packages/renderer/src/App.scss.js
@@ -1,12 +1,16 @@
 import 'animate.css'
 import 'cooltipz-css'
-import './scss/variables.css'
 import 'svelte-material-ui/bare.css'
 import 'winbox/src/css/winbox.css'
+
+import './scss/variables.css'
 import './scss/smui-theme-variables.scss'
+
 import './scss/materialTheme.scss'
 import './scss/bulma-style.scss'
 import './scss/styles.scss'
+import './scss/changelog.scss'
+
 import './scss/ck-editor.scss'
 import './scss/toast.scss'
-import './scss/changelog.scss'
+import './scss/gridjs.scss'
