4fd8ad3253f37fedf82332c66e6ed1c7a1061348
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-06-30 15:41:31 +0200

    Update Settings.svelte

--- packages/renderer/src/Pages/Settings.svelte
+++ packages/renderer/src/Pages/Settings.svelte
@@ -149,20 +149,19 @@
     const id = 'Settings'
     let display = window.db.get('active_tab') === id ? 'block' : 'none'
 
-    const buildPy = async () => {
-        window.stopServer()
-        const resources = window.path.resolve(window.ROOT_DIR, 'resources')
-        const config = window.path.resolve(resources, 'python_files/config.json')
-        const cmd = `conda run -n felionpy auto-py-to-exe -c ${config} -o ${resources}`
+    // const buildPy = async () => {
+    //     window.stopServer()
+    //     const resources = window.path.resolve(window.ROOT_DIR, 'resources')
+    //     const config = window.path.resolve(resources, 'python_files/config.json')
+    //     const cmd = `conda run -n felionpy auto-py-to-exe -c ${config} -o ${resources}`
 
-        const [output, error] = await window.exec(cmd)
-        if (error) {
-            console.error(error)
-            return
-        }
-        console.log(output)
-        window.startServer()
-    }
+    //     const [output, error] = await window.exec(cmd)
+    //     if (error) {
+    //         console.error(error)
+    //         return
+    //     }
+    //     window.startServer()
+    // }
 </script>
 
 <Changelog />
@@ -208,7 +207,7 @@
                             <button class="button is-link" on:click={() => (showServerControls = !showServerControls)}>
                                 Show server controls
                             </button>
-                            <button class="button is-warning" on:click={buildPy}>build</button>
+                            <!-- <button class="button is-warning" on:click={buildPy}>build</button> -->
                             {#if $developerMode}
                                 <div class="align">
                                     <Textfield bind:value={$pythonpath} label="Python path" style="width: 100%; " />
