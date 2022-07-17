d22ad17d0c1df30678c540ee412f57a04b6c21ae
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-07-17 17:33:51 +0200

    felionlib utils update

--- resources/python_files/felionlib/utils/__init__.py
+++ resources/python_files/felionlib/utils/__init__.py
@@ -3,3 +3,9 @@ def logger(*args, **kwargs):
         print(*args, kwargs, flush=True)
     else:
         print(*args, flush=True)
+
+
+def remove_special_chars_in_string(string: str, variables: list[str] = ["$", "^", "{", "}"]):
+    for var in variables:
+        string = string.replace(var, "")
+    return string
