92e38284c188a50816346ae34a9b1b8fa6337f8a
aravindhnivas <aravindhnivas28@gmail.com>
2022-07-25 17:12:32 +0200

    config update

--- packages/renderer/src/Pages/misc/Configs.svelte
+++ packages/renderer/src/Pages/misc/Configs.svelte
@@ -1,56 +1,59 @@
 <script lang="ts">
     import Textfield from '@smui/textfield';
     import { onDestroy } from 'svelte';
-    // export let active = ''
+    
     let CONFIGS = window.db.store()
     const unsubscribe = window.db.onDidAnyChange((newValue, oldValue) => {
-        // console.log({newValue, oldValue})
         CONFIGS = newValue
     })
+
     onDestroy(unsubscribe)
     
 </script>
 
 <div class="config_main__div box">
-    <Textfield
-    style="border: solid 1px #fff7;"
-        value={window.db.path}
-        label="CONFIGS location"
-        disabled
-        />
-        <div class="config__div ">
-            {#each Object.keys(CONFIGS) as label}
-                <div class="config_content">
-                    <Textfield bind:value={CONFIGS[label]} {label} />
-                    <button
-                        class="button is-success"
-                        on:click={()=>{
-                            window.db.set(label, CONFIGS[label])
-                            window.createToast('Saved', 'success')
-                        }}
-                            >Save</button>
-                    <button
-                        class="button is-warning"
-                        on:click={() => {
-                            window.db.delete(label)
-                            window.createToast(`${label} deleted`, 'danger')
-                        }}
-                        >Clear</button>
-                </div>
-                {:else}
-                <h1>No data</h1>
-            {/each}
-        </div>
-    
+    <div class="two_col__div">
+        <Textfield style="border: solid 1px #fff7;" value={window.db.path} label="CONFIGS location" disabled />
+        <i class="material-icons" on:click={() => window.shell.showItemInFolder(window.db.path)}>open_in_new</i>
+    </div>
+
+    <div class="config__div ">
+        {#each Object.keys(CONFIGS) as label}
+            <div class="config_content">
+                <Textfield bind:value={CONFIGS[label]} {label} />
+                <button
+                    class="button is-success"
+                    on:click={()=>{
+                        window.db.set(label, CONFIGS[label])
+                        window.createToast('Saved', 'success')
+                    }}
+                        >Save</button>
+                <button
+                    class="button is-warning"
+                    on:click={() => {
+                        window.db.delete(label)
+                        window.createToast(`${label} deleted`, 'danger')
+                    }}
+                    >Clear</button>
+            </div>
+            {:else}
+            <h1>No data</h1>
+        {/each}
+    </div>
+
     <div class="config_footer" style="margin-left: auto;">
-        <button
-        class="button is-danger"
+        <button class="button is-danger"
         on:click={() => window.db.clear()}>Clear all</button
         >
     </div>
 </div>
 
 <style lang="scss">
+    .two_col__div {
+        display: grid;
+        grid-auto-flow: column;
+        grid-template-columns: 1fr auto;
+    }
     .config_main__div {
         display: grid;
         grid-template-rows: auto 1fr auto;
