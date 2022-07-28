74a290c2e47bf89c9caf83e76696876254c92c71
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-07-28 17:50:35 +0200

    Update RateConstants.svelte

--- packages/renderer/src/Pages/timescan/components/controllers/RateConstants.svelte
+++ packages/renderer/src/Pages/timescan/components/controllers/RateConstants.svelte
@@ -1,13 +1,10 @@
-<script>
+<script lang="ts">
     import Textfield from '@smui/textfield'
     import TextAndSelectOptsToggler from '$components/TextAndSelectOptsToggler.svelte'
     import CustomSwitch from '$components/CustomSwitch.svelte'
+
     export let defaultInitialValues = true
     export let initialValues = ''
-    // export let ratek3 = ''
-    // export let k3Guess = ''
-    // export let ratekCID = ''
-    // export let kCIDGuess = ''
     export let kinetics_fitfile = ''
     export let configDir = ''
 </script>
@@ -15,10 +12,6 @@
 <div class="align h-center">
     <CustomSwitch bind:selected={defaultInitialValues} label="default" />
     <Textfield bind:value={initialValues} label="initialValues" />
-    <!-- <Textfield bind:value={ratek3} label="ratek3" />
-    <Textfield bind:value={k3Guess} label="k3Guess (min, max, step)" />
-    <Textfield bind:value={ratekCID} label="ratekCID" />
-    <Textfield bind:value={kCIDGuess} label="kCIDGuess (min, max, step)" /> -->
     <TextAndSelectOptsToggler
         bind:value={kinetics_fitfile}
         label="fit-config file (*.fit.json)"
