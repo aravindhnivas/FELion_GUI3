72030e5d95c7ac361898d72f9bcebd37bd6dfa78
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-07-28 12:59:45 +0200

    Update Test.svelte

--- packages/renderer/src/Pages/Test.svelte
+++ packages/renderer/src/Pages/Test.svelte
@@ -1,18 +1,5 @@
 <script lang="ts">
-    import Textfield from '@smui/textfield'
-    // import { persistentDB } from '$src/js/persistentDB'
-    // import { onDestroy } from 'svelte'
-
     const id = 'Test'
-
-    const db_test_value = window.persistentDB(id, 'test_value')
-    // onDestroy(() => {
-    //     unsubscribe()
-    // })
 </script>
 
-<section {id}>
-    <div class="align">
-        <Textfield bind:value={$db_test_value} label="Test" />
-    </div>
-</section>
+<section {id} />
