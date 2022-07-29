d843146423eb9a967566226a1e96fdebc5b3f2f3
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-07-29 21:05:02 +0200

    Create Notify.svelte

---
+++ packages/renderer/src/components/Notify.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let label: string
+    export let type: string = 'info'
+</script>
+
+{#if label}
+    <div class="block mt-5">
+        <span class="notification is-{type}">
+            {label}
+            <button
+                class="delete is-danger"
+                on:click={() => {
+                    label = ''
+                }}
+            />
+        </span>
+    </div>
+{/if}
