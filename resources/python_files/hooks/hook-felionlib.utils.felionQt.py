c90d1f9d80b9418ec2c610ee81f822aa72ecaf31
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-09-19 12:18:41 +0200

    adding matplotlib backends

---
+++ resources/python_files/hooks/hook-felionlib.utils.felionQt.py
@@ -0,0 +1,7 @@
+hiddenimports = [
+    "matplotlib.backends.backend_ps",
+    "matplotlib.backends.backend_agg",
+    "matplotlib.backends.backend_pdf",
+    "matplotlib.backends.backend_pgf",
+    "matplotlib.backends.backend_svg",
+]
