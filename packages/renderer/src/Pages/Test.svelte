83ddb9ced27425850255e43e80b0427e248ab191
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-11-18 16:11:50 +0100

    Update Test.svelte

--- packages/renderer/src/Pages/Test.svelte
+++ packages/renderer/src/Pages/Test.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import Tabs from '$src/components/DynamicTabs.svelte'
+    import DynamicTabs from '$src/components/DynamicTabs.svelte'
     const id = 'Test'
 </script>
 
 <section {id}>
-    <Tabs on:activeTabChange={(e) => console.log(e.detail)} />
+    <DynamicTabs on:activeTabChange={(e) => console.log(e.detail)} />
     <div class="container p-5">
         <p class="subtitle">This is a test page</p>
     </div>
