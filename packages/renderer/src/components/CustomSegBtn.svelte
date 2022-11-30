fcf4a68dc1d9555de13eefc32c5f1c18c0094bc5
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2022-11-30 13:19:55 +0100

    Create CustomSegBtn.svelte

---
+++ packages/renderer/src/components/CustomSegBtn.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import SegmentedButton, { Segment, Label } from '@smui/segmented-button'
+    export let choices: { name: string; selected: boolean }[] = []
+</script>
+
+<SegmentedButton segments={choices} let:segment key={(segment) => segment.name}>
+    <Segment
+        {segment}
+        selected={segment.selected}
+        on:click={() => {
+            segment.selected = !segment.selected
+            choices = choices // Remember to do this so Svelte knows that `choices` has changed.
+        }}
+    >
+        <Label>{segment.name}</Label>
+    </Segment>
+</SegmentedButton>
+
+<style lang="scss" global>
+    button.mdc-segmented-button__segment.mdc-ripple-upgraded {
+        background-color: var(--color-danger-light);
+        &.mdc-segmented-button__segment--selected {
+            background-color: var(--color-success-light);
+        }
+    }
+</style>
